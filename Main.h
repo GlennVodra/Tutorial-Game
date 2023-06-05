8033bdb52a3606fb309578f33a860199d36f0656
Glenn Vodra <37476686+GlennVodra@users.noreply.github.com>
2023-06-05 10:38:38 -0400

    Update Main.h

--- Main.h
+++ Main.h
@@ -1,23 +1,69 @@
 #pragma once
 
-#define GAME_NAME "Game"
+#define GAME_NAME "CGame"
 
 #define GAME_RES_WIDTH  384
 #define GAME_RES_HEIGHT 240
 #define GAME_BPP        32
 #define GAME_DRAWING_AREA_MEMORY_SIZE (GAME_RES_WIDTH * GAME_RES_HEIGHT * (GAME_BPP / 8))
 
-typedef struct GAMEBITMAP{
-    BITMAPINFO BitmapInfo;
-    void* Memory;
-} GAMEBITMAP ;
+#define CALCULATE_AVERGAE_FPS_EVERY_X_FRAMES 128
+#define TARGET_MICROSECONDS_PER_FRAME 16667
+
+#define SIMD
+
+#pragma warning(disable : 4820) //Memory padding
+#pragma warning(disable : 5045) //Spectre/Meltdown Vulnerability
+
+typedef LONG(NTAPI* _NtQueryTimerResolution) (OUT PULONG MinimumResolution, OUT PULONG MaximumResolution, OUT PULONG CurrentResolution);
+_NtQueryTimerResolution NtQueryTimerResolution;
+
+typedef struct GAMEBITMAP {
+	BITMAPINFO BitmapInfo; 
+	void* Memory;          
+} GAMEBITMAP;
 
 typedef struct PIXEL32{
-    uint8_t Blue;
-    uint8_t Green;
-    uint8_t Red;
-    uint8_t Alpha;
-}PIXEL32;
+	uint8_t Blue;
+	uint8_t Green;
+	uint8_t Red;
+	uint8_t Alpha;
+} PIXEL32;
+
+typedef struct GAMEPERFDATA {
+	uint64_t TotalFramesRendered;
+	float RawFPSAverage;
+	float CookedFPSAverage;
+	int64_t PerfFrequency;
+	MONITORINFO MonitorInfo;
+	int32_t MonitorWidth;
+	int32_t MonitorHeight;
+	BOOL DisplayDebugInfo;
+	ULONG MinimumTimerResolution;
+	ULONG MaximumTimerResolution;
+	ULONG CurrentTimerResolution;
+	DWORD HandleCount;
+	PROCESS_MEMORY_COUNTERS_EX MemInfo;
+	SYSTEM_INFO SystemInfo;
+	int64_t CurrentSystemTime;
+	int64_t PreviousSystemTime;
+	FILETIME ProcessCreationTime;
+	FILETIME ProcessExitTime;
+	int64_t CurrentUserCPUTime;
+	int64_t CurrentKernelCPUTime;
+	int64_t PreviousUserCPUTime;
+	int64_t PreviousKernelCPUTime;
+	double CPUPercent;
+} GAMEPERFDATA;
+
+typedef struct PLAYER {
+	char Name[12];
+	int32_t WorldPosX;
+	int32_t WorldPosY;
+	int32_t HP;
+	int32_t Strength;
+	int32_t MP;
+} PLAYER;
 
 LRESULT CALLBACK MainWindowProc(_In_ HWND WindowHandle, _In_ UINT Message, _In_ WPARAM wParam, _In_ LPARAM lParam);
 
@@ -25,6 +71,12 @@ DWORD CreateMainGameWindow(void);
 
 BOOL GameIsAlreadyRunning(void);
 
-void  ProcessPlayerInput(void);
+void ProcessPlayerInput(void);
 
 void RenderFrameGraphics(void);
+
+#ifdef SIMD
+void ClearScreen(_In_ __m128i* Color);
+#else 
+void ClearScreen(_In_ PIXEL32* Color);
+#endif
